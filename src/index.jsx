22c33b55c6df84f889e143fec85bade0609e734d
Rishabh Kanaujia <kumar.rishabh007.rk@gmail.com>
2019-05-13 15:55:50 +0530

    restructuring files

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App.jsx"
+import HomePage from "./pages/HomePage.jsx"
 
-ReactDOM.render(<App/>,document.getElementById("root"));
+ReactDOM.render(<HomePage/>,document.getElementById("root"));
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+
+class HomePage extends Reac
+export default App;
